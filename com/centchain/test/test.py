b45fda35169fbdcca3facf7fa4c57f4120ea7384
yzbin <jiabin126email@126.com>
2017-11-12 16:54:16 +0800

    test robin's computer

---
+++ com/centchain/test/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/12 16:48
+# @Author  : robin
+# @Site    : 
+# @File    : test.py
+# @Software: PyCharm Community Edition
+
+
+if __name__ == '__main__':
+    print(True)
