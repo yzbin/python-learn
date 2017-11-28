b4e51179ee1d970bf5d9efa5f92ca3e2b123ed0b
yzbin <jiabin126email@126.com>
2017-11-28 20:05:03 +0800

    test robin's computer now

--- com/robin/debug/do_try.py
+++ com/robin/debug/do_try.py
@@ -1,27 +1,21 @@
-<<<<<<< Updated upstream
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Time    : 2017/11/28 10:40
 # @Author  : bin
-=======
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # @Time    : 2017/11/28 7:20
 # @Author  : robin
->>>>>>> Stashed changes
 # @Site    : 
 # @File    : do_try.py
 # @Software: PyCharm Community Edition
 
-<<<<<<< Updated upstream
-=======
 # 程序运行的过程中，如果发生了错误，可以事先约定返回一个错误代码，这样，就可以知道是否有错，以及出错的原因。
 # 在操作系统提供的调用中，返回错误码非常常见。比如打开文件的函数open()，
 # 成功时返回文件描述符（就是一个整数），出错时返回-1
 
 # 用错误码来表示是否出错十分不便，因为函数本身应该返回的正常结果和错误码混在一起，
 # 造成调用者必须用大量的代码来判断是否出错：
->>>>>>> Stashed changes
 
 # def foo():
 #     r = some_function()
@@ -36,11 +30,9 @@
 #         print('Error')
 #     else:
 #         pass
-# <<<<<<< Updated upstream
-# =======
+
 #
-# # 一旦出错，还要一级一级上报，直到某个函数可以处理该错误（比如，给用户输出一个错误信息）。
-# >>>>>>> Stashed changes
+# # 一旦出错，还要一级一级上报，直到某个函数可以处理该错误（比如，给用户输出一个错误信息）
 # # 所以高级语言通常都内置了一套try...except...finally...的错误处理机制，Python也不例外
 #
 # # try
@@ -56,8 +48,6 @@
 # finally:
 #     print('finally...')
 # print('END')
-#
-# <<<<<<< Updated upstream
 # 当我们认为某些代码可能会出错时，就可以用try来运行这段代码，如果执行出错，则后续代码不会继续执行，
 # 而是直接跳转至错误处理代码，即except语句块，执行完except后，如果有finally语句块，则执行finally语句块，至此，执行完毕。
 #
@@ -134,18 +124,4 @@ def main():
 # 也就是说，不需要在每个可能出错的地方去捕获错误，只要在合适的层次去捕获错误就可以了。
 # 这样一来，就大大减少了写try...except...finally的麻烦。
 
-
-
-
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> Stashed changes
 
